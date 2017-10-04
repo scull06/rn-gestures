9d9b02ed35247ddcd84bc1da45556a5e7fd9e871
scull06 <scull06@gmail.com>
2017-10-04 17:12:48 +0200

    Android movement support is ok now

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ import { NativeModules } from 'react-native';
 const { RNGestures } = NativeModules;
 
 export default RNGestures;
+
+/*
+USAGE
+
+Registering Lsiteners :   RNGestures.registerMovementDetection(3, 2000);
+
+Unregistering Events: RNGestures.stopMovementDetection();
+
+DeviceEventEmitter.addListener("MOVING", () => {
+    // Do Stuff
+});
+
+DeviceEventEmitter.addListener("STATIONARY", () => {
+    // Do Stuff
+});
+
+*/
